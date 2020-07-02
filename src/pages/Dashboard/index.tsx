ce97193dea37d258abb6b9b3d355834da78c3baa
rodrigo <rodrigo.rgtic@gmail.com>
2020-07-02 11:59:56 -0300

    feat: save localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -18,7 +18,15 @@ interface IRepository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<IRepository[]>([]);
+  const [repositories, setRepositories] = useState<IRepository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+    if(storagedRepositories){
+      return JSON.parse(storagedRepositories)
+    }else{
+      return [];
+    }
+  });
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement> ): Promise<void> {
     // evitar reload da página.
@@ -41,6 +49,11 @@ const Dashboard: React.FC = () => {
       setInputError('Erro na busca por esse repositório');      
     }
   }
+
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories));
+  },[repositories]);
 
 
   
